4a241bed4ed6d2732b9cab09761e4b6f8bd6bd64
SeanAlexanderHarris <sean.harris@cinch.co.uk>
2022-01-23 12:40:35 +0000

    try different twitter card image

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -9,7 +9,7 @@ const siteMetadata = {
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
-  socialBanner: '/static/images/twitter-card.png',
+  socialBanner: '/static/images/twitter-card.jpeg',
   email: 'sean.alexander.harris.29@googlemail.com',
   github: 'https://github.com/SeanAlexanderHarris',
   twitter: 'https://twitter.com/SeanHAlexander',
