0479b2cb03f92c87eb9670453fc58b1d797e19b3
SeanAlexanderHarris <sean.harris@cinch.co.uk>
2022-01-23 21:49:15 +0000

    add disqus config for comments functionality

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -5,7 +5,7 @@ const siteMetadata = {
   description: "Thoughts on what I'm building, learning, reading, & riding",
   language: 'en-uk',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
+  siteUrl: 'https://www.seandevs.com',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
