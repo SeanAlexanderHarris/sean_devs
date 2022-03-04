1c676f47d777a33fbbfc4b7b9ca4893813c7a4e2
SeanAlexanderHarris <sean.harris@cinch.co.uk>
2022-03-04 06:17:31 +0000

    final draft of ddd & naming blog post, addedunder construction project data

--- data/projectsData.js
+++ data/projectsData.js
@@ -3,14 +3,14 @@ const projectsData = [
     title: 'Sefton Park Social',
     description: `A web app to help people organise activities in Sefton Park, South Liverpool`,
     imgSrc: '/static/images/sefton_park_skyview.jpeg',
-    href: 'https://github.com/SeanAlexanderHarris/sefton-park-social',
+    href: '/blog/the-time-machine',
   },
-  // {
-  //   title: 'JOGOL Ride',
-  //   description: `Cycle from John O Groats to Lands End, tip to tail of this sceptred isle in one long rolling ride.`,
-  //   imgSrc: '/static/images/time-machine.jpg',
-  //   href: '/blog/the-time-machine',
-  // },
+  {
+    title: 'JOGOL Ride',
+    description: `My plan to cycle from John O Groats to Lands End`,
+    imgSrc: '/static/images/ribble_endurance_ti.jpg',
+    href: '/blog/the-time-machine',
+  },
 ]
 
 export default projectsData
